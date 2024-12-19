48e26dbecfa857eab0edbe40d713ac04ea0ffd81
Umair <umairibnfarooq1@gmail.com>
2024-12-19 15:36:46 +0530

    user in signup

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
   },
   userType: {
     type: String,
-    enum: ['admin', 'agent', 'hr','blog writer'],
+    enum: ['admin', 'agent', 'hr','blog writer','user'],
     required: [true, 'Please provide user type']
   },
   leadAccess: {
